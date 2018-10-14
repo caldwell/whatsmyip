4ec2028b32e3cfe7c1621e6a0d59f13b5341fcea
David Caldwell <david@porkrind.org>
2018-10-14 14:31:11 -0700

    Aborted Rocket.rs attempt

--- whatsmyipd.rs
+++ whatsmyipd.rs
@@ -1,19 +1,27 @@
 // Copyright © 2018 David Caldwell <david@porkrind.org>
-extern crate iron;
-extern crate chrono ;
 
-use chrono::prelude::*;
-use iron::prelude::*;
-use iron::status;
-use std::env;
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+use rocket::{Request, Response};
+use rocket::http::Status;
+use std::io::Cursor;
+
+//#[get("/")]
+#[catch(404)]
+fn index<'a>(req: &Request) -> Response<'a> {
+    Response::build()
+        .raw_header("X-Source-URL", "https://github.com/caldwell/whatsmyip")
+        .sized_body(Cursor::new(format!("{}", req.remote().unwrap_or("0.0.0.0:0".parse().unwrap()).ip())))
+        .status(Status::Ok)
+        .finalize()
+}
 
 fn main() {
-    fn handler(req: &mut Request) -> IronResult<Response> {
-        let ip = req.remote_addr.ip().to_string();
-        println!("{:35} IP: {}", Local::now(), ip);
-        let mut resp = Response::with((status::Ok, ip));
-        resp.headers.append_raw("X-Source-URL", b"https://github.com/caldwell/whatsmyip".to_vec());
-        Ok(resp)
-    }
-    let _server = Iron::new(handler).http(format!("0.0.0.0:{}", env::var("WHATSMYIP_PORT").unwrap_or("3141".to_string()))).unwrap();
+    println!("Error: {}", rocket::ignite()
+//             .mount("/", routes![index])
+             .catch(catchers![index])
+             .launch());
 }
